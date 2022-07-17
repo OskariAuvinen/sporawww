87d4925613b14876095a71ee57493a6aa8684045
Oskari Auvinen <yakkieh@hotmail.com>
2022-07-17 17:24:09 +0300

    cleaning Panel.stories.js file from excess code

--- src/Components/Panel/Panel.stories.js
+++ src/Components/Panel/Panel.stories.js
@@ -1,28 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-
 import { Panel } from 'react95';
 
-const Wrapper = styled.div`
-  padding: 5rem;
-  background: ___CSS_0___;
-  #default-buttons button {
-    margin-bottom: 1rem;
-    margin-right: 1rem;
-  }
-
-  #cutout {
-    background: ___CSS_1___;
-    padding: 1rem;
-    width: 300px;
-  }
-`;
-
-export default {
-    title: 'Panel',
-    component: Panel,
-    decorators: [story => <Wrapper>{story()}</Wrapper>]
-};
 
 export const GreyBox = ( { h1 }) => (
     <Panel
@@ -33,7 +11,7 @@ export const GreyBox = ( { h1 }) => (
         <h1 style={{ padding: '0.5rem' }}>
             {h1}
         </h1>
-
+        
     </Panel>
 );
 
