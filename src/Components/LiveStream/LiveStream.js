0b06f496ce6d2a9c2d51ff9bcf18eb5ac597d72b
puppee <pupp3@icloud.com>
2022-09-16 12:06:16 +0300

    Livestream lisätty

--- src/Components/LiveStream/LiveStream.js
+++ src/Components/LiveStream/LiveStream.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavBar } from '../AppBar/AppBar.stories';
 import { Headline } from '../Headline/Headline';
 import './LiveStream.css'
+import ReactHlsPlayer from 'react-hls-player';
+
 
 export const LiveStream = () => {
 
@@ -15,9 +17,13 @@ export const LiveStream = () => {
         <div className="LiveStream">
             <div className="LiveStreamContainer">
                 <Headline info={info} />
-            <div className="StoryCardsContainer">
-
-            </div>
+                <ReactHlsPlayer
+                    src="https://17fc8e548d71.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.356299520911.channel.kmWXdgRoLRJD.m3u8"
+                    autoPlay={true}
+                    controls={true}
+                    width="100%"
+                    height="auto"
+                />
                 <NavBar />
             </div>
         </div>
