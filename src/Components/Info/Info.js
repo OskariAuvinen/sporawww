d46de6ff676894f67bda11825a6bdbdb5dd82125
Oskari Auvinen <yakkieh@hotmail.com>
2022-09-23 21:02:35 +0300

    minor info changes and details for production

--- src/Components/Info/Info.js
+++ src/Components/Info/Info.js
@@ -18,26 +18,27 @@ export const Info = () => {
                 </div>
                 <div className="InfoWrapper">
                     <div className="InfoTextHeadline">
-                        Etkot ja Jatkot
+                        Etkot ja Jatkot:
                     </div>
                     <div className="InfoText">
-                        Tapahtuman virallisina etko- ja jatkopaikkoina toimivat Mikonkadun ravintolat Henry's Pub sekä On The Rocks. 
+                        Tapahtuman virallisina etko- ja jatkopaikkoina toimivat Mikonkadun ravintolat Henry's Pub sekä On The Rocks.
                     </div>
                     <div className="InfoTextHeadline">
                         Matkan aikana
                     </div>
                     <div className="InfoText">
-                        Lasipullot ja erilliset juoma-astiat ovat kiellettyjä ja näistä kiinni jäädessään ei neuvotella. Tee siis drinkkisi ennakkoon muovipulloihin ja otathan pienemmät juomat tölkkeinä mukaan.
+                        Lasipullot ja erilliset juoma-astiat, paitsi tapahtuman virallinen VIP-ämpäri, ovat kiellettyjä ja näistä kiinni jäädessään ei neuvotella. Tee siis drinkkisi ennakkoon muovipulloihin ja otathan pienemmät juomat tölkkeinä mukaan.
                     </div>
                     <div className="InfoTextHeadline">
-                        Edut
+                        Edut:
                     </div>
                     <div className="InfoText">
-                        Ajon aikana kusitauot pyritään järjestämään n. tunnin välein yllä mainituissa baareissa. WC:ssä käynti aiheuttaa humalaa, koska se edellyttää osaa teitä wc:ssä käyviä ostamaan 2cl shotin, jonka olemme saaneet tarjoushinnalla 3,25e.
+                    Ajon aika tyrät rytkyää,  rakko hölskyy ja vessaan on päästävä. Niinpä rytmitämme tauot mikonkadulle noin tunnin välein, jossa voitte käyttää yllämainittujen juottoloiden wc-tiloja. Ja jotta hyvä juoma ei haihtuisi kaupunkimme viemäriverkostoon ja kuppilat hyötyisi tästä, ostakaa vessakäynnin yhteydessä, edes osa teistä, tarjoushintainen 2cl shotti 3.25e hinnalla.
+                        <p></p>
                         <br></br>
-                        <p>On The Rocks 2cl Salmaria.</p>
+                        <p>On The Rocks myy Salmaria hintaan 3.25e.</p>
                         <br></br>
-                        <p>Henry's Pub 2cl Fisua.</p>
+                        <p>Henry's Pub myy Fisua hintaan 3.25e.</p>
                         <br></br>
                         <p>Tarjous on voimassa tiskillä matkalippua näyttämällä ajon aikana.</p>
                     </div>
